d014d57c9bf7ddd7377ba85787aba8576b3de6ff
thepwnexperts <thepwnexperts@gmail.com>
2023-02-25 17:40:37 +0000

    default file /swapfile and size 4 GB / 4096 MB

--- linux/vps/swap/swap.sh
+++ linux/vps/swap/swap.sh
@@ -2,13 +2,16 @@
 #Create swap on VPS using bash script via arguments
 #Parse command-line arguments
 
-while getopts ":s:f:" opt; do
+while getopts ":s:f:F:" opt; do
 case $opt in
 s)
 SWAP_SIZE=$OPTARG
 ;;
 f)
 SWAP_FILE=$OPTARG
+;;
+F)
+ADD_TO_FSTAB=$OPTARG
 ;;
 ?)
 echo "Invalid option: -$OPTARG" >&2
@@ -20,28 +23,26 @@ exit 1
 ;;
 esac
 done
-#Check if swap size and file were provided
 
-if [ -z "$SWAP_SIZE" ] || [ -z "$SWAP_FILE" ]; then
-echo "Swap size or file name not provided. Exiting."
-exit 1
-fi
+#Use default values if swap size or file name not provided
+SWAP_SIZE=${SWAP_SIZE:-4096}
+SWAP_FILE=${SWAP_FILE:-/swapfile}
+ADD_TO_FSTAB=${ADD_TO_FSTAB:-true}
+
 #Check if swap already exists
-
 if swapon -s | grep -q "$SWAP_FILE"; then
 echo "Swap file $SWAP_FILE already exists. Exiting."
 exit
 fi
-#Create the swap file
 
+#Create the swap file
 sudo fallocate -l ${SWAP_SIZE}M "$SWAP_FILE"
 sudo chmod 600 "$SWAP_FILE"
 sudo mkswap "$SWAP_FILE"
 sudo swapon "$SWAP_FILE"
-#Ask user if they want to add swap file to /etc/fstab
 
-read -p "Do you want to add the swap file to /etc/fstab? (y/n) " ANSWER
-if [[ "$ANSWER" =~ ^[Yy]$ ]]; then
+#Add swap file to /etc/fstab if specified
+if [[ $ADD_TO_FSTAB == true ]]; then
 echo "$SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab
 echo "Swap file added to /etc/fstab."
 else
@@ -49,3 +50,4 @@ echo "Swap file not added to /etc/fstab."
 fi
 
 echo "Swap file $SWAP_FILE created successfully."
+
