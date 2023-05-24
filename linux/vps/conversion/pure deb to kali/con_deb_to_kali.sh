00ab3939308810c5d393c99530f844d55463089a
thepwnexperts <123484962+thepwnexperts@users.noreply.github.com>
2023-05-24 12:23:13 +0530

    permission issue fixed
    permission issue fixed via sudo

--- linux/vps/conversion/pure deb to kali/con_deb_to_kali.sh
+++ linux/vps/conversion/pure deb to kali/con_deb_to_kali.sh
@@ -2,28 +2,28 @@
 
 # Add Kali Linux repository
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" | sudo tee /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
 
 # Update repositories
-apt-get update
-apt dist-upgrade -y
+sudo apt-get update
+sudo apt dist-upgrade -y
 
 # Install packages
-apt-get install -y htop bpytop neofetch
+sudo apt-get install -y htop bpytop neofetch
 
 # Install additional packages
-apt-get install -y npm nodejs
-npm install -g pm2 uuid
+sudo apt-get install -y npm nodejs
+sudo npm install -g pm2 uuid
 
 # Download and run swap script
 wget https://raw.githubusercontent.com/thepwnexperts/advance-things/main/linux/vps/swap/swap.sh
-chmod +x swap.sh
+sudo chmod +x swap.sh
 ./swap.sh
 
 # Download and run update script
 wget https://raw.githubusercontent.com/thepwnexperts/advance-things/main/linux/vps/debian/auto/update_cron/update.sh
-chmod +x update.sh
-./update.sh
+sudo chmod +x update.sh
+sudo ./update.sh
 
 # Clean up downloaded scripts
 rm swap.sh update.sh
