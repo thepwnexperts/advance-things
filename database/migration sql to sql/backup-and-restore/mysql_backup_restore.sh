5bbef42afaf8e76e84b18ef5249b52eb8d4c8b97
thepwnexperts <123484962+thepwnexperts@users.noreply.github.com>
2023-06-13 10:48:09 +0530

    restore may not work
    restore may not work  properly prefer to use source

--- database/migration sql to sql/backup-and-restore/mysql_backup_restore.sh
+++ database/migration sql to sql/backup-and-restore/mysql_backup_restore.sh
@@ -9,24 +9,37 @@ read -p "Enter your choice (1 or 2): " choice
 if [ "$choice" == "1" ]; then
     # Backup operation
     read -p "Enter MySQL username: " username
-    read -sp "Enter MySQL password: " password
+
+    # Read the MySQL password securely
+    echo -n "Enter MySQL password: "
+    read -s password
     echo
+
     read -p "Enter backup file name: " backup_file
 
     # Perform backup
-    mysqldump -u "$username" -p"$password" --all-databases > "$backup_file"
+    mysqldump -u "$username" --password="$password" --all-databases > "$backup_file"
 
     echo "Backup completed successfully!"
 
 elif [ "$choice" == "2" ]; then
     # Restore operation
     read -p "Enter MySQL username: " username
-    read -sp "Enter MySQL password: " password
+
+    # Read the MySQL password securely
+    echo -n "Enter MySQL password: "
+    read -s password
     echo
+
     read -p "Enter backup file name: " backup_file
 
-    # Perform restore
-    mysql -u "$username" -p"$password" < "$backup_file"
+    # Perform restore excluding the 'mysql' database
+    mysql -u "$username" --password="$password" -e "SET FOREIGN_KEY_CHECKS=0; SET UNIQUE_CHECKS=0; SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';" 
+
+    for db in $(mysql -u "$username" --password="$password" -e "SHOW DATABASES;" | awk '{ if ($1 != "Database" && $1 != "information_schema" && $1 != "performance_schema" && $1 != "mysql" && $1 != "sys") print $1 }'); do
+        echo "Restoring database: $db"
+        mysql -u "$username" --password="$password" "$db" < "$backup_file"
+    done
 
     echo "Restore completed successfully!"
 
