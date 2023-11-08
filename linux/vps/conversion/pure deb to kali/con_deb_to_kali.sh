515fef4cad41304dfd24d054e19823d7501f0187
vikas kumawat <42940952+Vikaskumawat2018@users.noreply.github.com>
2023-11-08 09:37:35 +0530

    Update con_deb_to_kali.sh
    path update for auto update

--- linux/vps/conversion/pure deb to kali/con_deb_to_kali.sh
+++ linux/vps/conversion/pure deb to kali/con_deb_to_kali.sh
@@ -21,7 +21,7 @@ sudo chmod +x swap.sh
 ./swap.sh
 
 # Download and run update script
-wget https://raw.githubusercontent.com/thepwnexperts/advance-things/main/linux/vps/debian/auto/update_cron/update.sh
+wget https://github.com/thepwnexperts/advance-things/blob/main/linux/vps/conversion/pure%20deb%20to%20kali/con_deb_to_kali.sh
 sudo chmod +x update.sh
 sudo ./update.sh
 
